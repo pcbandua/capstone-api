051aee1208071e21d15f7827261baba26dc16184
Paulo Bandua <pcbandua@gmail.com>
2024-10-30 11:49:02 -0700

    added Apprentice model data with seeds csv method

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "csv"
+
+csv_file_path = Rails.root.join("db", "apprentice.csv")
+
+CSV.foreach(csv_file_path, headers: true) do |row|
+  Apprentice.create!(
+    email: row["email"],
+    password_digest: row["password_digest"],
+    address: row["address"],
+    education: row["education"],
+    holland_code: row["holland_code"],
+  )
+end
