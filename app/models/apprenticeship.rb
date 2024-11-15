0812fde2124fa4b394745b1e6291501c2f91dee2
Paulo Bandua <pcbandua@gmail.com>
2024-11-14 17:20:11 -0800

    added new 100 apprenticeships + employers

--- app/models/apprenticeship.rb
+++ app/models/apprenticeship.rb
@@ -2,6 +2,6 @@ class Apprenticeship < ApplicationRecord
   has_many :applications
   belongs_to :employer
 
-  has_secure_password
-  validates :email, presence: true, uniqueness: true
+  # has_secure_password
+  # validates :email, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ CSV.foreach(csv_file_path, headers: true) do |row|
     holland_code: row["holland_code"],
   )
 end
+pp "test1"
 
 employer_csv_file_path = Rails.root.join("db", "employer.csv")
 
@@ -36,7 +37,45 @@ CSV.foreach(employer_csv_file_path, headers: true) do |row|
   )
 end
 
-apprenticeship_csv_file_path = Rails.root.join("db", "apprenticeship.csv")
+pp "test2"
+
+employer_csv_file_path = Rails.root.join("db", "generated_employers.csv")
+
+CSV.foreach(employer_csv_file_path, headers: true) do |row|
+  Employer.find_or_create_by!(
+    email: row["email"],
+    password_digest: row["password_digest"],
+    company_name: row["company_name"],
+    company_logo: row["company_logo"],
+    description: row["description"],
+    industry: row["industry"],
+    website: row["website"],
+  )
+end
+
+pp "test3"
+
+# apprenticeship_csv_file_path = Rails.root.join("db", "apprenticeship.csv")
+
+# CSV.foreach(apprenticeship_csv_file_path, headers: true) do |row|
+#   Apprenticeship.find_or_create_by!(
+#     employer_id: row["employer_id"],
+#     title: row["title"],
+#     summary: row["summary"],
+#     location_status: row["location_status"],
+#     skills_required: row["skills_required"],
+#     qualifications: row["qualifications"],
+#     holland_code_preference: row["holland_code_preference"],
+#     compensation: row["compensation"],
+#     duration: row["duration"],
+#     positions_available: row["positions_available"],
+#     national_registered_status: row["national_registered_status"],
+#   )
+# end
+
+pp "test4"
+
+apprenticeship_csv_file_path = Rails.root.join("db", "updated_100_generated_apprenticeships.csv")
 
 CSV.foreach(apprenticeship_csv_file_path, headers: true) do |row|
   Apprenticeship.find_or_create_by!(
@@ -51,42 +90,5 @@ CSV.foreach(apprenticeship_csv_file_path, headers: true) do |row|
     duration: row["duration"],
     positions_available: row["positions_available"],
     national_registered_status: row["national_registered_status"],
-  )
-end
-
-apprenticeship_csv_file_path = Rails.root.join ("db", "100_generated_apprenticeships.csv")
-
-CSV.foreach(apprenticeship_csv_file_path, headers:true) do |row|
-  Apprenticeship.find_or_create_by!(
-    employer_id: row["employer_id"],
-    title: row["title"],
-    summary: row["summary"],
-    location_status: row["location_status"],
-    skills_required: row["skills_required"],
-    qualifications: row["qualifications"],
-    holland_code_preference: row["holland_code_preference"],
-    compensation: row["compensation"],
-    duration: row["duration"],
-    positions_available: row["positions_available"],
-    national_registered_status: row["national_registered_status"],
-  )
-end
-
-CSV.foreach(apprenticeship_csv_file_path, headers: true) do |row|
-  apprenticeship = Apprenticeship.find_or_initialize_by(
-    employer_id: row["employer_id"],
-    title: row["title"]
-  )
-  
-  apprenticeship.update!(
-    summary: row["summary"],
-    location_status: row["location_status"],
-    skills_required: row["skills_required"],
-    qualifications: row["qualifications"],
-    holland_code_preference: row["holland_code_preference"],
-    compensation: row["compensation"],
-    duration: row["duration"],
-    positions_available: row["positions_available"],
-    national_registered_status: row["national_registered_status"]
   )
 end
